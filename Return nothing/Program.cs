a852ac6459926152ca83e3243ec318cb0111c37a
NT-000 <nicolai.thue.reier@gmail.com>
2024-11-16 21:26:16 +0100

    update

---
+++ Return nothing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Return_nothing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            printMsg();
+        }
+
+        public static void printMsg()
+        {
+            Console.WriteLine("Denne metoden returnerer ingenting");
+        }
+    }
+}
